8fd09ee7aadef1e316b8d8354010bf2d824323f4
Perla20 <perlablancoq@hotmail.com>
2017-11-16 18:49:21 -0500

    añadiendo funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
-var area = document.getElementById("insert-links");
-var boton = document.getElementById("save");
-var lista = document.getElementById("container-links");
-boton.addEventListener("click",function(event){
-  // verifico que hay texto
-  if(area.value){
-    // creamos li contenedor es li y queremos que sea link y un ancor
-    var li = document.createElement("li");
-    var link=document.createElement("a");
-    link.setAttribute("href","#");
-    link.texContent = area.value;
+var area = document.getElementById('insert-links');
+var boton = document.getElementById('save');
+var lista = document.getElementById('container-links');
+
+boton.addEventListener('click', function(event) {
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  if (area.value) {
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+
+    link.setAttribute('href', '#');
+
+    link.textContent = area.value;
 
     li.appendChild(link);
+
     lista.appendChild(li);
-    // el value te tra el contenido
-    area.value= " ";
+
+    area.value = '';
   }
-  // console.log(event.target);
 });
